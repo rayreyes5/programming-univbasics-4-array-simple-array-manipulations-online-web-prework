9775021e0040e8a7fd3412e69e46821307c75b7a
Ray Reyes <rayreyes5@gmail.com>
2019-09-04 01:20:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,8 +1,9 @@
 array.push(elements)
  updated_array = using_push(colors_in_the_rainbow, next_color
 array.unshift
-
+ @updated_array = using_unshift(bouroughs_in_nyc, @new_neighborhood)
 array.pop
+
 array.pop_with_args
 array.shift
 array.shift_with_args
